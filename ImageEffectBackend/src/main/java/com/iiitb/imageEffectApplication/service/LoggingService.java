c6b79cf060359d90327367cb6cc11a876d7f5389
Ajitesh Kumar Singh <Ajitesh.Kumar@iiitb.ac.in>
2023-12-29 00:11:20 +0530

    Logging left

--- ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/service/LoggingService.java
+++ ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/service/LoggingService.java
@@ -7,26 +7,170 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+
 @Service
 public class LoggingService {
 
     public void addLog(String fileName, String effectName, String optionValues) {
+
+        LocalDateTime localDateTime = LocalDateTime.now();
+        String currentTime = localDateTime.toString();
+
+        try
+        {
+            File logFile = new File("logfile.txt");
+
+            FileWriter fileWriter = new FileWriter("logfile.txt", true);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+            bufferedWriter.write(fileName + " " + effectName + " " + optionValues + " " + currentTime);
+            bufferedWriter.newLine();
+
+            bufferedWriter.close();
+            fileWriter.close();
+        }
+
+        catch (IOException e)
+        {
+            System.out.println("Exception occurred while writing to log file.");
+            System.out.println(e);
+        }
+
         return;
     }
 
     public List<LogModel> getAllLogs() {
-        return new ArrayList<LogModel>();
+
+        List<LogModel> allLogs = new ArrayList<LogModel>();
+
+        try
+        {
+            File logFile = new File("logfile.txt");
+
+            FileReader fileReader = new FileReader(logFile);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            String line;
+
+            while ((line = bufferedReader.readLine()) != null)
+            {
+                String sp[] = line.split(" ");
+                String fileName = sp[0];
+                String effectName = sp[1];
+                String optionValues = sp[2];
+                String timestamp = sp[3];
+
+                LogModel logModel = new LogModel(timestamp, fileName, effectName, optionValues);
+                allLogs.add(logModel);
+            }
+        }
+
+        catch (IOException e)
+        {
+            System.out.println("Error occurred while reading log file.");
+            System.out.println(e);
+        }
+
+        return allLogs;
     }
 
     public List<LogModel> getLogsByEffect(String effectName) {
-        return new ArrayList<LogModel>();
+
+        List<LogModel> logsByEffect = new ArrayList<LogModel>();
+
+        try
+        {
+            File logFile = new File("logfile.txt");
+
+            FileReader fileReader = new FileReader(logFile);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            String line;
+
+            while ((line = bufferedReader.readLine()) != null)
+            {
+                String sp[] = line.split(" ");
+                String fileName = sp[0];
+                String effect = sp[1];
+                String optionValues = sp[2];
+                String timestamp = sp[3];
+
+                if (effect.equalsIgnoreCase(effectName))
+                {
+                    LogModel logModel = new LogModel(timestamp, fileName, effect, optionValues);
+                    logsByEffect.add(logModel);
+                }
+            }
+        }
+
+        catch (IOException e)
+        {
+            System.out.println("Error occurred while reading log file.");
+            System.out.println(e);
+        }
+
+        return logsByEffect;
     }
 
     public void clearLogs() {
+
+        try
+        {
+            new FileWriter("logfile.txt", false).close();
+        }
+
+        catch (IOException e)
+        {
+            System.out.println("Error occurred while deleting logs.");
+            System.out.println(e);
+        }
+
         return;
     }
 
     public List<LogModel> getLogsBetweenTimestamps(LocalDateTime startTime, LocalDateTime endTime) {
-        return new ArrayList<LogModel>();
+
+        List<LogModel> logsBetweenTimestamps= new ArrayList<LogModel>();
+
+        try
+        {
+            File logFile = new File("logfile.txt");
+
+            FileReader fileReader = new FileReader(logFile);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            String line;
+
+            while ((line = bufferedReader.readLine()) != null)
+            {
+                String sp[] = line.split(" ");
+                String fileName = sp[0];
+                String effectName = sp[1];
+                String optionValues = sp[2];
+                String timestamp = sp[3];
+
+                LocalDateTime effectTimeStamp = LocalDateTime.parse(timestamp);
+
+                if (effectTimeStamp.isAfter(startTime) && effectTimeStamp.isBefore(endTime))
+                {
+                    LogModel logModel = new LogModel(timestamp, fileName, effectName, optionValues);
+                    logsBetweenTimestamps.add(logModel);
+                }
+            }
+        }
+
+        catch (IOException e)
+        {
+            System.out.println("Error occurred while reading log file.");
+            System.out.println(e);
+        }
+
+        return logsBetweenTimestamps;
     }
 }
