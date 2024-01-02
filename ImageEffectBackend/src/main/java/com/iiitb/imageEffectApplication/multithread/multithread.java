104d0d8fb88e14a27ff43d1cca836dc9dab3b083
Ajitesh Kumar Singh <Ajitesh.Kumar@iiitb.ac.in>
2024-01-02 17:45:05 +0530

    comments in multithreading

--- ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/multithread/multithread.java
+++ ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/multithread/multithread.java
@@ -1,16 +1,22 @@
 package com.iiitb.imageEffectApplication.multithread;
 import com.iiitb.imageEffectApplication.service.LoggingService;
 
+
+// This class is used to create a thread for adding logs to the database
 public class multithread extends Thread{
     public LoggingService lx;
     public String fname , ename , opval;
+
+    // Constructor
     public multithread(LoggingService lx , String fname , String ename , String opval){
+
         this.lx = lx;
         this.fname = fname;
         this.ename = ename;
         this.opval = opval;
     }
 
+    // This method is used to add logs to the database
     @Override public void run(){
         try{
             lx.addLog(fname, ename, opval);
